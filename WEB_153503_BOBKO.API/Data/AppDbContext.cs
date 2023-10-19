6a8504c740d55c55566ca2e94997557979491745
hulahup01 <ilya.deepak@yandex.ru>
2023-10-20 00:15:28 +0300

    Add project API

---
+++ WEB_153503_BOBKO.API/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WEB_153503_BOBKO.Domain.Entities;
+
+namespace WEB_153503_BOBKO.API.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<Game> Games { get; set; }
+
+        public DbSet<GameGenre> GameGenres { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+    }
+}
